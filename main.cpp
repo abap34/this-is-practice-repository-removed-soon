60fce0251f1c893509c9fe0f6b5e0612162dbb6f
abap34 <uchi_ymgc@her0.jp>
2024-04-02 15:23:58 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char* argv[]) {
-    std::ifstream file(argv[1]);
+#include <string>
 
+
+int main(int argc, char *argv[])
+{
+    std::ifstream input(argv[1]);
+    std::ofstream output(argv[2]);
     std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << "!" << std::endl;
+    while (std::getline(input, line))
+    {
+        line += "!";
+        output << line << "\n";
     }
 
-    file.close();
-    
+    input.close();
+    output.close();
+
     return 0;
 }
