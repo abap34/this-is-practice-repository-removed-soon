22cc7d32bd07228f25f192cd8e8e058ce5be673d
abap34 <abap0002@gmail.com>
2024-09-28 23:14:28 +0900

    make fast `try_to_sum` ???

--- src/mylib.cpp
+++ src/mylib.cpp
@@ -1,10 +1,13 @@
 #include "mylib.hpp"
+#include <unistd.h>
 
 int sum(int a, int b, int c) {
     return a + b + c;
 }
 
 int try_to_sum(int a, int b, int c) {
+    // wait 0.1 seconds
+    usleep(0.1 * 1000 * 1000);
     if (a + b + c > 100) {
         return -1;
     }
