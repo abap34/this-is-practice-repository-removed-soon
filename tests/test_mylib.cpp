eb8b385f1cf7564af7521e43b69fad24f728b25e
abap34 <abap0002@gmail.com>
2024-09-28 16:48:35 +0900

    Update test_mylib.cpp

--- tests/test_mylib.cpp
+++ tests/test_mylib.cpp
@@ -5,6 +5,10 @@ TEST(MyLibTest, SumTest) {
     EXPECT_EQ(sum(1, 2, 3), 6);
 }
 
+TEST(MyLibTest, MustFail) {
+    EXPECT_EQ(sum(1, 2, 3), 7);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
