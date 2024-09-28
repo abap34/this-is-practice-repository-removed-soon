ec8605f06c77855746e959c17a80b8bb3f25c6a1
github-action-benchmark <github@users.noreply.github.com>
2024-09-28 14:32:47 +0000

    add Benchmark (googlecpp) benchmark result for c899b551ffd12a935a5dbd05896583c9112c8509

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727533840076,
+  "lastUpdate": 1727533967355,
   "repoUrl": "https://github.com/abap34/this-is-practice-repository-removed-soon",
   "entries": {
     "Benchmark": [
@@ -134,6 +134,36 @@ window.BENCHMARK_DATA = {
             "value": 50090939.99862671,
             "unit": "ns/iter",
             "extra": "iterations: 100\ncpu: 17064.600000000006 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abap0002@gmail.com",
+            "name": "Yuchi Yamaguchi",
+            "username": "abap34"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c899b551ffd12a935a5dbd05896583c9112c8509",
+          "message": "Merge pull request #5 from abap34/fast-impl\n\nmake fast `try_to_sum` ???",
+          "timestamp": "2024-09-28T23:31:51+09:00",
+          "tree_id": "4aec37e223988270fbcb573bdbd0bcf7e79711c2",
+          "url": "https://github.com/abap34/this-is-practice-repository-removed-soon/commit/c899b551ffd12a935a5dbd05896583c9112c8509"
+        },
+        "date": 1727533967073,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_Sum",
+            "value": 50094304.08477783,
+            "unit": "ns/iter",
+            "extra": "iterations: 100\ncpu: 18508.370000000006 ns\nthreads: 1"
           }
         ]
       }
