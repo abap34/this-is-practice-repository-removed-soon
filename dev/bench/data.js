39c6c454f20d56e22d8647ca9aa163430969d9a6
github-action-benchmark <github@users.noreply.github.com>
2024-09-28 14:22:16 +0000

    add Benchmark (googlecpp) benchmark result for 499e66ce1c4f0f0542193ec023e33c215901dca9

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727533336910,
+  "repoUrl": "https://github.com/abap34/this-is-practice-repository-removed-soon",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "abap34",
+            "username": "abap34"
+          },
+          "committer": {
+            "name": "abap34",
+            "username": "abap34"
+          },
+          "id": "499e66ce1c4f0f0542193ec023e33c215901dca9",
+          "message": "make fast `try_to_sum` ???",
+          "timestamp": "2024-09-28T11:56:45Z",
+          "url": "https://github.com/abap34/this-is-practice-repository-removed-soon/pull/5/commits/499e66ce1c4f0f0542193ec023e33c215901dca9"
+        },
+        "date": 1727533336076,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_Sum",
+            "value": 100110106.46820068,
+            "unit": "ns/iter",
+            "extra": "iterations: 100\ncpu: 27444.78 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
